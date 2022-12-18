8ce56f4d3f39d98b48951f2eb793b286ef988f11
Yuliia Cherkashyna <jul11091109@gmail.com>
2022-12-18 23:55:24 +0200

    changes

--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -17,10 +17,12 @@ export default function ImageGalleryItem({ image, openModal }) {
 
 
 ImageGalleryItem.propTypes = {
-  image: PropTypes.object.isRequired,
-  webformatURL: PropTypes.string,
-  tags: PropTypes.string,
-  largeImageURL: PropTypes.string,
+
+    image: PropTypes.shape({
+      webformatURL: PropTypes.string.isRequired,
+      tags: PropTypes.string.isRequired,
+      largeImageURL: PropTypes.string.isRequired,
+  }).isRequired,
+
   openModal: PropTypes.func.isRequired,
 }
-
